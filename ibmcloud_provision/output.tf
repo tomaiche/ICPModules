2b6cc874da841210941d7167d4b467bfc2037c1a
tomaiche <thomas_aiche@fr.ibm.com>
2018-10-16 18:35:07 +0200

    Update output.tf

--- ibmcloud_provision/output.tf
+++ ibmcloud_provision/output.tf
@@ -1,1 +1,2 @@
 output "dependsOn" { value = "${null_resource.vm-create_done.id}" description="Output Parameter when Module Complete"}
+output "ipv4" { value = "${ibm_compute_vm_instance.softlayer_virtual_guest.ipv4_address}"}
