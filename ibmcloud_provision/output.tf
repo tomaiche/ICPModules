65f84c2b580b7dcb6d382695f8f7e9cdaf779556
tomaiche <thomas_aiche@fr.ibm.com>
2018-10-19 15:20:34 +0200

    Update output.tf

--- ibmcloud_provision/output.tf
+++ ibmcloud_provision/output.tf
@@ -1,9 +1,9 @@
 output "dependsOn" { value = "${null_resource.vm-create_done.id}" description="Output Parameter when Module Complete"}
 output "ipv4" {
-                value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address}" 
+                value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address_private}" 
                 description="IPV4 Address when Module Complete"
               }
-output "ipv4_private" {
-                value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address_private}" 
-                description="IPV4 Private Address when Module Complete"
-              }
+# output "ipv4_private" {
+#                 value = "${ibm_compute_vm_instance.softlayer_virtual_guest.*.ipv4_address_private}" 
+#                 description="IPV4 Private Address when Module Complete"
+#              }
